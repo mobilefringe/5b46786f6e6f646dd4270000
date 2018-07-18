9b92f0618b266c468d3e2793d09275a6c5070547
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-07-18 15:49:24 -0400

    Place Bathurst 1.0  /application.js

--- application.js
+++ application.js
@@ -701,7 +701,9 @@ function renderPromoDetails(container, template, collection, mall_name){
             
             // English Image
             if (val.promo_image_url_abs.indexOf('missing.png') > 0){
-                val.image_url = "https://placehold.it/500x500";
+                val.show_img = "display: none;"
+            } else {
+                val.image_url = promo_image_url_abs
             }
             // French Image
             if (val.promo_image2_url_abs.indexOf('missing.png') > 0){
